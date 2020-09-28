b94067a93cf9adfd0d1b06a99887d16978d8f903
Guilherme Lara <54341844+Guilherme-A-Lara@users.noreply.github.com>
2020-09-27 21:41:00 -0300

    update has256
    estuturação para padronização com os outros algoritmos

--- hash256-224.py
+++ hash256-224.py
@@ -1,14 +1,33 @@
 import hashlib
+
+
 def encrypt_string(hash_string):
     sha_signature = \
         hashlib.sha256(hash_string.encode()).hexdigest()
     return sha_signature
+
+
 def decrypt_string(hash_string):
     sha_signature_decrypt = \
         hashlib.sha256(hash_string.encode()).hexdigest()
     return sha_signature_decrypt
-hash_string = input('Digite sua Senha: ')
-sha_signature = encrypt_string(hash_string)
-sha_signature_decrypt = decrypt_string(hash_string)
-print(sha_signature)
-print(sha_signature_decrypt)
+
+
+entrar = input('Deseja fazer login S/N : ')
+if entrar == 'S' or entrar == 's':
+    hash_string = input('Digite sua Senha: ')
+    sha_signature = encrypt_string(hash_string)
+    sha_signature_decrypt = decrypt_string(hash_string)
+    print('Senha criptografada : ')
+    print('--------------------------')
+    print(sha_signature[:8])
+    print('--------------------------')
+    ver = input('Deseja ver a senha descriptografada S/N ')
+    if ver == 'S' or ver == 's':
+        print('--------------------------')
+        print(f'Sua senha e : {hash_string}')
+        print('--------------------------')
+    else:
+        print('Saindo ...')
+else:
+    print('Saindo ...')

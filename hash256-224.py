d5e093ccbccbbe37103d1af9c83b08be06f54942
Guilherme Lara <54341844+Guilherme-A-Lara@users.noreply.github.com>
2020-09-27 21:43:50 -0300

    uptade
    mudança na logica, nao precisa da def de decrypto

--- hash256-224.py
+++ hash256-224.py
@@ -7,17 +7,10 @@ def encrypt_string(hash_string):
     return sha_signature
 
 
-def decrypt_string(hash_string):
-    sha_signature_decrypt = \
-        hashlib.sha256(hash_string.encode()).hexdigest()
-    return sha_signature_decrypt
-
-
 entrar = input('Deseja fazer login S/N : ')
 if entrar == 'S' or entrar == 's':
     hash_string = input('Digite sua Senha: ')
     sha_signature = encrypt_string(hash_string)
-    sha_signature_decrypt = decrypt_string(hash_string)
     print('Senha criptografada : ')
     print('--------------------------')
     print(sha_signature[:8])
